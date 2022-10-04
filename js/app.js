c86fa8e79dee31da0286c21f23fb3e943e81b667
Alexis Franco <ronaldofranco196@gmail.com>
2022-10-03 22:27:43 -0300

    segundo ejercicio2

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const nuevaLista = [
+    'Alexis',
+    26,
+    false,
+    new Date('september 01 1996'),
+    miLibro = {
+        titulo: 'El Alquimista',
+        autor: 'Paulo Cohelo',
+        fecha: 1988,
+        url: 'https://www.planetadelibros.com/libro-el-alquimista/119671',
+    }
+]
+console.log(nuevaLista)
